1b6b771b80ba54d6c40f91c4f7ff01a9a1c148a6
Rounak Banik <rounakbanik@gmail.com>
2016-08-21 06:49:25 +0530

    Write preliminary solution.

---
+++ week4/scc.py
@@ -0,0 +1,70 @@
+graph = {}
+reverse = {}
+
+
+def DFSLoop(graph):
+    global t
+    t = 0
+    global s
+    s = None
+    global explored
+    explored = {}
+    global leader
+    leader = {}
+    global f
+    f = {}
+    for i in range(9, 0, -1):
+        if i not in explored:
+            s = i
+            DFS(graph, i)
+            
+def DFS(graph, i):
+    global t
+    explored[i] = True
+    leader[i] = s
+
+    for j in graph[i]:
+        if j not in explored:
+            DFS(graph, j)
+
+    t = t+1
+    f[i] = t
+
+def Construct(graph, f):
+    leader_graph = {}
+
+    for key in reverse:
+        if f[key] not in leader_graph:
+            print key
+            print f[key]
+            for ele in graph[key]:
+                leader_graph[f[key]] = [f[ele]]
+        else:
+            for ele in graph[key]:
+                leader_graph[f[key]].append(ele)
+            
+    return leader_graph  
+            
+    
+
+with open("exam.txt") as f:
+    for line in f:
+        line = line.split()
+        line = [int(i) for i in line]
+        if line[1] not in graph:
+            graph[line[1]] = [line[0]]
+        else:
+            graph[line[1]].append(line[0])
+
+        if line[0] not in reverse:
+            reverse[line[0]] = [line[1]]
+        else:
+            reverse[line[0]].append(line[1])
+
+
+DFSLoop(graph)
+reverse_graph= Construct(reverse, f)
+DFSLoop(reverse_graph)
+
+print leader
+
